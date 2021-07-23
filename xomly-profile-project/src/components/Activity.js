3e4c58b2650dce839be8d0ed4a7714fa9b9eb0b8
Bradon Nichols <nichols.bradon@gmail.com>
2021-07-23 14:25:49 -0600

    save 3

--- xomly-profile-project/src/components/Activity.js
+++ xomly-profile-project/src/components/Activity.js
@@ -1,4 +1,4 @@
-import React, {Component, useReducer} from 'react'
+import React, {Component, Fragment, useReducer} from 'react'
 import { useParams } from 'react-router-dom'
 
 class Activity extends Component {
@@ -27,6 +27,13 @@ class Activity extends Component {
 
         var {isLoaded, user} = this.state
 
+        var activityIcons = {
+            'calendar' : 'https://preview.pixlr.com/images/800wm/100/1/1001503340.jpg',
+            'briefcase' : 'https://media.istockphoto.com/vectors/briefcase-icon-vector-flat-briefcase-symbol-illustration-vector-vector-id1176590128',
+            'check-circle' : 'https://cdns.iconmonstr.com/wp-content/assets/preview/2018/240/iconmonstr-check-mark-circle-thin.png',
+            'credit-card' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsaW-awAWbkIj0kt7d1oy1-oFsI4FmwGmoyw&usqp=CAU'
+        }
+
         if(!isLoaded) {
             return <div>Loading...</div>
         }
@@ -35,10 +42,14 @@ class Activity extends Component {
         return (
             <div className='Activity'>
                <p>Recent Activity</p>
-               <p>{user.recentActivity.map((activity) => (
-                   <p>{activity.date} {activity.details}</p>
-                ))}
-                </p>
+               <div>
+                    {user.recentActivity.map((activity) => (
+                        <div>
+                            <img src={activityIcons[activity.icon]} height="25px" width="25px"/> 
+                            {activity.date} <b>{activity.details}</b>
+                        </div>
+                    ))}
+                </div>
                <button>ALL ACTIVITIES</button>
             </div>
         )
--- xomly-profile-project/src/components/Header.js
+++ xomly-profile-project/src/components/Header.js
@@ -1,8 +1,8 @@
-
+// import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 const Header = () => {
     return <div className='Header'>
-        <p>add button</p>
+        {/* <Link to='add'><AddCircleOutlineIcon/></Link> */}
         <p>Contact Details</p>
         <p>Bell Icon</p>
         <img className='small-pic' src='https://i.pinimg.com/236x/07/33/ba/0733ba760b29378474dea0fdbcb97107.jpg'/>
